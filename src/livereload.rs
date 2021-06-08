b906ebf658d0f03e72e3e9637157287bd05be29b
Peter Malmgren <ptmalmgren@gmail.com>
2021-06-08 14:47:38 -0400

    Add in handshake

--- src/livereload.rs
+++ src/livereload.rs
@@ -1,16 +1,77 @@
-use futures_util::StreamExt;
+use futures_util::{SinkExt, StreamExt};
 
-use warp::ws::WebSocket;
+use warp::ws::{Message, WebSocket};
 use warp::Filter;
 
-async fn user_connected(ws: WebSocket) {
-    let (_wstx, mut wsrx) = ws.split();
+use serde::{Deserialize, Serialize};
+// use serde_json::Result as SerdeResult;
 
-    while let Some(result) = wsrx.next().await {
-        if let Ok(msg) = result {
-            log::info!("Received message: {:?}", msg);
+#[derive(Serialize, Deserialize)]
+struct ClientHandshake {
+    command: String,
+    protocols: Vec<String>,
+    ver: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ServerHandshake {
+    command: String,
+    protocols: Vec<String>,
+}
+
+enum ClientState {
+    Handshake(ClientHandshake),
+    // optional: store information here about the client
+    // Ready,
+    Invalid,
+}
+
+async fn user_connected(ws: WebSocket) {
+    let (mut wstx, mut wsrx) = ws.split();
+    let result = wsrx.next().await;
+    let state: ClientState = match result {
+        Some(Ok(msg)) => match msg.to_str() {
+            Err(_e) => ClientState::Invalid,
+            Ok(msg_str) => match serde_json::from_str::<ClientHandshake>(msg_str) {
+                Err(_e) => ClientState::Invalid,
+                Ok(handshake) => ClientState::Handshake(handshake),
+            },
+        },
+        None => {
+            log::error!("Received null message");
+            ClientState::Invalid
         }
-    }
+        Some(Err(e)) => {
+            log::error!("error receiving message: {}", e);
+            ClientState::Invalid
+        }
+    };
+
+    match state {
+        ClientState::Handshake(client_handshake) => {
+            if !client_handshake
+                .protocols
+                .contains(&"http://livereload.com/protocols-official-7".to_string())
+            {
+                log::error!("we can only handle version 7 of livereload");
+                return;
+            }
+            let resp = ServerHandshake {
+                command: "hello".to_string(),
+                protocols: vec!["http://livereload.com/protocols-official-7".to_string()],
+            };
+            let resp_text = serde_json::to_string(&resp).unwrap();
+            wstx.send(warp::ws::Message::text(resp_text.as_str()))
+                .await
+                .unwrap();
+        }
+        _ => {
+            log::error!("error during livereload handshake, closing connection");
+            return;
+        }
+    };
+
+    while let Some(_msg) = wsrx.next().await {}
 }
 
 pub async fn server() {
@@ -25,7 +86,42 @@ pub async fn server() {
 }
 
 mod test {
-    #[test]
-    fn test_it_handshakes() {
+    use super::*;
+    use warp;
+
+    #[tokio::test]
+    async fn test_it_handshakes() {
+        let route = warp::ws()
+            .map(|ws: warp::ws::Ws| ws.on_upgrade(move |socket: WebSocket| user_connected(socket)));
+
+        let mut client = warp::test::ws()
+            .path("livereload")
+            .handshake(route)
+            .await
+            .expect("handshake");
+
+        let handshake = ClientHandshake {
+            command: "hello".to_string(),
+            protocols: vec![
+                "http://livereload.com/protocols-official-6".to_string(),
+                "http://livereload.com/protocols-official-7".to_string(),
+            ],
+            ver: "3.3.2".to_string(),
+        };
+        client
+            .send(Message::text(
+                serde_json::to_string_pretty(&handshake).unwrap().as_str(),
+            ))
+            .await;
+        let resp = client.recv().await.expect("recv");
+        assert!(resp.is_text());
+        let resp_handshake: ServerHandshake =
+            serde_json::from_str(resp.to_str().unwrap()).expect("serialization error");
+
+        assert_eq!(resp_handshake.command, "hello".to_string());
+        assert_eq!(
+            resp_handshake.protocols[0],
+            "http://livereload.com/protocols-official-7".to_string()
+        );
     }
 }
